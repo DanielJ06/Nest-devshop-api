f02f2e9787f8e7f4eebdcedc2b5ef93d80685a9b
danieljesus <daniel.j.rodrigues06@gmail.com>
2020-12-16 10:01:42 -0300

    .env files with config module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,17 +1,23 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 import { GraphQLModule } from '@nestjs/graphql';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { async } from 'rxjs';
 import { Category } from './modules/category/category.entity';
 import { CategoryModule } from './modules/category/category.module';
 
 @Module({
   imports: [
-    TypeOrmModule.forRoot({
-      type: 'postgres',
-      url: 'postgres://postgres:daniel06@localhost:5433/devshop',
-      autoLoadEntities: true,
-      synchronize: true,
-      entities: [Category],
+    ConfigModule.forRoot({ isGlobal: true }),
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: async (configService: ConfigService) => ({
+        type: 'postgres',
+        url: configService.get('DATABASE_URL'),
+        autoLoadEntities: true,
+        synchronize: true,
+      }),
     }),
     GraphQLModule.forRoot({
       autoSchemaFile: 'schema.gql',
